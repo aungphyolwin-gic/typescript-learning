98b7a36ee1b4eb482b9a01dc504dfa1ef53ac35b
AungPhyoLwin <aungphyolwin@gicjp.com>
2024-03-04 15:52:22 +0630

    funcitons

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,30 @@
-let person:string = "aung aung";
-person = "Mg Mg";
-// person = 34;
+// functions
 
+//simple function
 
-let isValid:boolean = true;
-isValid = false;
-// isValid = "hello";
-
-let number:number = 34;
-number = 3.4;
-// number = 'haha';
-
-//null and undefined
-let something: null = null;
-// something = 'null';
-let anotherThing: undefined = undefined;
-// anotherThing = 'hello';
-
-console.log('name ' + person);
-
-
-//------------------
-//----Arrays--------
-//------------------
-let fruits: string[] = ['apple','mango', 'oragne'];
-// fruits.push(23);
-fruits.push('banana');
-console.log(fruits);
-
-let ages: number[] = [23,25,24,21];
-ages.push(20);
-
-console.log(ages);
-
-// Array Interfere
-let  animals = ['dog','cat','bird'];
-animals.push('snake');
-const f = animals[3];
-
-let mixData = ['hello', true, null, undefined, 3.4, undefined, 34];
-mixData.push("Another");
-
-const val = mixData[4]
-
-
-//-----------------
-//-----Objects Literal-----
-//-----------------
-
-let user: { firstname: string, age: number} = { firstname: "May", age:22}
-user.firstname = "Nicky";
-
-//------------------------------------
-//Object Interfere with Object Literal
-//------------------------------------
-let Person = {
-    name : 'Ma Ma',
-    age : 25,
-    gender : 'female'
+function addTwoNumber(a:number,b: number){
+    return a+b;     //return any type of data
 }
 
-Person.age = 28;
-const age = Person.age;
-console.log(Person);
+const subTwoNumber = (a:number, b: number):number => {
+    return a-b;
+}
+
+let result = addTwoNumber(2,3);
+result = subTwoNumber(5,2);
+
+function addAllNumber(items: number[]):void{
+    const total = items.reduce((a,b)=> a+b, 0);
+    console.log(total)
+}
+
+addAllNumber([2,3,4,5,6,7,7,3]);
+
+//--------------------------------
+//Return Type Interfere
+//--------------------------------
+function foramtGreeting(name:string , greeting: string): string{
+    return `${greeting} ${name}`;
+}
+
+console.log(foramtGreeting("Ko Ko","Mingalarpar"));
