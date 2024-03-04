1c45c01d23ba06f58e3dd1a04990eda955ffbf51
AungPhyoLwin <aungphyolwin@gicjp.com>
2024-03-04 14:08:41 +0630

    Basic Data Type

---
+++ index.ts
@@ -0,0 +1,39 @@
+let person:string = "aung aung";
+person = "Mg Mg";
+// person = 34;
+
+
+let isValid:boolean = true;
+isValid = false;
+// isValid = "hello";
+
+let number:number = 34;
+number = 3.4;
+// number = 'haha';
+
+//null and undefined
+let something: null = null;
+// something = 'null';
+let anotherThing: undefined = undefined;
+// anotherThing = 'hello';
+
+console.log('name ' + person);
+
+let fruits = ['apple','mango', 'oragne'];
+// fruits.push(23);
+fruits.push('banana');
+console.log(fruits);
+
+let ages = [23,25,24,21];
+ages.push(20);
+
+console.log(ages);
+
+let Person = {
+    name : 'Mg Mg',
+    age : 30,
+    gender : 'male'
+}
+
+Person.age = 28;
+console.log(Person);
