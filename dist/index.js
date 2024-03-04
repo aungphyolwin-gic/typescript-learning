c71e08180c31d3038739b083d7cc50acb4d19ebb
AungPhyoLwin <aungphyolwin@gicjp.com>
2024-03-04 15:52:42 +0630

    any Data Type

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,19 @@
 "use strict";
-// functions
-//simple function
-function addTwoNumber(a, b) {
-    return a + b; //return any type of data
+// Any Type
+let variable;
+let anotherVariable;
+variable = 'hello';
+variable = 27;
+anotherVariable = true;
+anotherVariable = { name: 'hello', age: 22 };
+//any type in array
+const array1 = ['hi', 12, true, null];
+array1.push({ gender: 'male' });
+//function
+function addTwoValue(data) {
+    return data + data;
 }
-const subTwoNumber = (a, b) => {
-    return a - b;
-};
-let result = addTwoNumber(2, 3);
-result = subTwoNumber(5, 2);
-function addAllNumber(items) {
-    const total = items.reduce((a, b) => a + b, 0);
-    console.log(total);
-}
-addAllNumber([2, 3, 4, 5, 6, 7, 7, 3]);
-//--------------------------------
-//Return Type Interfere
-//--------------------------------
-function foramtGreeting(name, greeting) {
-    return `${greeting} ${name}`;
-}
-console.log(foramtGreeting("Ko Ko", "Mingalarpar"));
+console.log(addTwoValue(2));
+console.log(addTwoValue("hI"));
+//useful when migrating js to ts
+//using any won't cause any errors initially
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
-// functions
+// Any Type
 
-//simple function
+let variable: any
+let anotherVariable 
 
-function addTwoNumber(a:number,b: number){
-    return a+b;     //return any type of data
+variable = 'hello';
+variable = 27;
+
+anotherVariable = true;
+anotherVariable = { name: 'hello', age: 22}
+
+//any type in array
+const array1: any[] = ['hi', 12, true, null]
+array1.push({gender:'male'})
+
+
+//function
+function addTwoValue(data: any):any{
+    return data+data;
 }
 
-const subTwoNumber = (a:number, b: number):number => {
-    return a-b;
-}
+console.log(addTwoValue(2));
+console.log(addTwoValue("hI"));
 
-let result = addTwoNumber(2,3);
-result = subTwoNumber(5,2);
-
-function addAllNumber(items: number[]):void{
-    const total = items.reduce((a,b)=> a+b, 0);
-    console.log(total)
-}
-
-addAllNumber([2,3,4,5,6,7,7,3]);
-
-//--------------------------------
-//Return Type Interfere
-//--------------------------------
-function foramtGreeting(name:string , greeting: string): string{
-    return `${greeting} ${name}`;
-}
-
-console.log(foramtGreeting("Ko Ko","Mingalarpar"));
+//useful when migrating js to ts
+//using any won't cause any errors initially
